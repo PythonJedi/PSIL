a3e932b36015a54b2c26545341dddf48324393f4
Python Jedi <t.c.hewittjr@gmail.com>
2015-04-10 02:50:36 -0500

    Interpreter environment manipulation now compliant

--- psil.py
+++ psil.py
@@ -22,72 +22,55 @@ class Interpreter:
             for op in self:
                 if isinstance(op, parse.Push):
                     self.env.stack.push(op.value)
+                    
                 elif isinstance(op, parse.NewExpression):
                     self.arg_len_stack.append(0)
+                    
                 elif isinstance(op, parse.Execute):
                     value = self.env.stack.pop()
-                    if isinstance(value, data.LLCode):
-                        value(self)
-                    elif isinstance(value, data.Code):
+                    
+                    if isinstance(value, data.Code):
                         self.append_env()
                         self.push(iter(value))
+                    
                     elif isinstance(value, data.Reference):
+                        code = self.search(value)
+                        assert isinstance(code, data.Code)
+                    
+                        if isinstance(code, data.LLCode):
+                            code(self)
+                        
+                        else:
+                            self.append_env(value)
+                            self.push(iter(code))
                         
         except Exception as e:
             #print(self.env.stack)
             raise e # need the stack trace for debugging!
             
-    def op(self, ins):
-        """Execute a single instruction."""
-        if isinstance(ins, parse.Execute):
-            self.op_stream_stack.append(self.op_stream)
-            code = self.env.stack.pop()
-            
-            if not isinstance(code, data.Code):
-                raise TypeError("Tried to execute non-Code object "+str(code))
-            elif isinstance(code, data.LLCode):
-                print(code)
-                print(self.env.stack)
-                print(self.arg_len_stack)
-                code.run(self) # LLCode is a superclass of any code written in python
-            else:
-                if hasattr(code, "name"): # executing named code literal
-                    self.build_search_path(code.name)
-                    print(code.name)
+    def append_env(self, reference=None):
+        """Appends a new environment.
+        
+        If a reference is given the new environment is appended below the 
+        namespace pointed to by that reference, with the search path pointing up
+        through the reference."""
+        if reference: # set search path
+            start = self.search_up(reference)
+            for name in reference:
+                if start.validate(name):
+                    start = start.get(name)
+                    start.parent = self.env
                 else:
-                    print("Anonymous")
-                print(self.env.stack)
-                print(self.arg_len_stack)
-                self.append_env(data.Namespace(self.env, data.Stack(self.arg_len_stack.pop()-1, self.env.stack)))
-                
-                self.op_stream_stack.append(code.instructions())
-                return True
-            
-        elif isinstance(ins, parse.Push):
-            if isinstance(ins.value, parse.Reference):
-                ins.value = self.env.deref(ins.value) # dereference
-            self.env.stack.push(ins.value)
-            if self.arg_len_stack:
-                self.arg_len_stack[-1] += 1
-            else:
-                self.arg_len_stack.append(1) # starting out
-        return False
-            
-    def build_search_path(self, name):
-        """Appends the namespaces in name to the search path. 
+                    raise AttributeError("Failed to find "+str(name))
+                self.env = start
         
-        should only be called with a verified name."""
-        trav = self.env.search_up(name)
-        name = name.next() # need to move to the next name as the first was found
-        
-        while name.names:
-            if trav.validate(name[0]):
-                self.append_env(trav.grab(name[0]))
-            else:
-                raise AttributeError("Failed to find "+str(name))
-            trav = self.env
+        # Add new namespace
+        self.env = data.Namespace(self.env,
+                                  data.Stack(self.arg_len_stack.pop(),
+                                             self.env.stack)
+                                 )
                 
-    def clean_search_path(self):
+    def pop_env(self):
         """Moves the environment back to the previous execution namespace.
         
         first moves the current env reference up the search path and then
@@ -95,14 +78,11 @@ class Interpreter:
         reference)."""
         self.env = self.env.parent
         while self.env and not self.env.stack:
+            temp = self.env
             self.env = self.env.parent
-                
-    def append_env(self, env):
-        assert not self.env == env
-        env.parent = self.env
-        self.env = env
+            temp.parent = None
         
-    def deref(self, name):
+    def search(self, name):
         if not name.string: # empty ref
             return self
         nm = name.copy()
